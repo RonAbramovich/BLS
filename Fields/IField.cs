cfd01ce7ea47255c9b71a2798c1de35fe1d186cf
ronab <ronab@DESKTOP-APP474F>
2026-02-21 14:09:33 +0200

    Add project files.

---
+++ Fields/IField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BLS.Fields
+{
+    public interface IField<TElement> where TElement : IFieldElement<TElement>
+    {
+        int Characteristic { get; }  // p
+        int ExtensionDegree { get; } // 1 for Fp, k for Fp^k
+
+        // Canonical elements
+        TElement Zero { get; }
+        TElement One { get; }
+
+        TElement FromInt(long value);
+
+        bool IsValid(TElement x);
+    }
+}
---
+++ Fields/IFieldElement.cs
@@ -0,0 +1,15 @@
+﻿namespace BLS.Fields
+{
+    public interface IFieldElement<TSelf> where TSelf : IFieldElement<TSelf> // TSelf is concrete type - for avoiding boxing and enabling static dispatch
+    {
+        TSelf Add(TSelf other);
+        TSelf Sub(TSelf other);
+        TSelf Multiply(TSelf other);
+        TSelf AdditiveInverse();
+        TSelf MultiplicativeInverse();                // multiplicative inverse (error if zero)
+        TSelf Power(long exponent);
+
+        bool IsZero { get; }
+        bool Equals(TSelf other);
+    }
+}
